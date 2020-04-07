cc8406a79ae2d3199dd5ea985fee398191f650f5
nedele <m17320495060@163.com>
2020-04-07 22:23:48 +0800

    创建了StringHomework类，其中包含GetCount方法和mimicJoin方法，完成了GetCount方法的单元测试和代码编写

---
+++ HomeWorkTest/StringHomeworkTest.cs
@@ -0,0 +1,27 @@
+using homework;
+using NUnit.Framework;
+
+namespace HomeWorkTest
+{
+    public class StringTest
+    //给Stringhomework的两个方法做单元测试
+    {
+        //两个都不难，也没啥关系，貌似不用设置setup，直接Test
+        [Test]
+        public void GetCountTest()
+        {
+            StringHomework test = new StringHomework();
+
+            Assert.AreEqual(1, test.GetCount("abc", "a"));
+            Assert.AreEqual(2, test.GetCount("abcabc", "a"));
+            Assert.AreEqual(3, test.GetCount("abcabcabc", "a"));
+            Assert.AreEqual(3, test.GetCount("abcabcabc", "b"));
+            Assert.AreEqual(3, test.GetCount("abcabcabc", "c"));
+            Assert.AreEqual(0, test.GetCount("abcabcabc", "d"));
+        }
+
+
+
+
+    }
+}
---
+++ homework/StringHomework.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace homework
+{
+    public class StringHomework
+    {
+        //实现GetCount(string container, string target)方法，可以统计出container中有多少个target
+        public int GetCount(string container, string target)
+        {
+      bool Ishas = container.Contains(target);
+            if (!Ishas)
+            {
+                return 0;
+            }
+            else
+            {
+                string recontainer=container.Replace(target, string.Empty);
+                return (container.Length - recontainer.Length) / target.Length;//想到了用replace和empty，但没有想到用length
+            }
+               
+        }
+    }
+}
